b778315e6ef4a5abe5d55ac7d5e23a6ade4b10f4
matiascorrea007 <matiascorreautn@gmail.com>
2016-05-03 04:51:36 +0200

    add nombre de user al loguear
    -nombre de user al loguear y link de logout en user

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -26,6 +26,8 @@
   <div class="navbar-inner">
     <div class="container"> <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"><span
                     class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span> </a><a class="brand" href="index.html">Bootstrap Admin Template </a>
+        <!--comprueva si esta logueado y se carga el panel de user-->
+        @if (Auth::check())
       <div class="nav-collapse">
         <ul class="nav pull-right">
           <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">
@@ -35,18 +37,21 @@
               <li><a href="javascript:;">Help</a></li>
             </ul>
           </li>
-          <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown"><i
-                            class="icon-user"></i> EGrappler.com <b class="caret"></b></a>
+          
+          <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">
+          <i class="icon-user"></i> {{ Auth::user()->usu_nombre }} <b class="caret"></b></a>
             <ul class="dropdown-menu">
               <li><a href="javascript:;">Profile</a></li>
-              <li><a href="javascript:;">Logout</a></li>
+              <li><a href="{{ url('/logout') }}">Logout</a></li>
             </ul>
           </li>
+
         </ul>
         <form class="navbar-search pull-right">
           <input type="text" class="search-query" placeholder="Search">
         </form>
       </div>
+      @endif
       <!--/.nav-collapse --> 
     </div>
     <!-- /container --> 
