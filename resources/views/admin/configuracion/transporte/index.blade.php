342808a000ce8503064867905695dd3f021debd2
matiascorrea007 <matiascorreautn@gmail.com>
2016-05-17 07:41:44 +0200

    agregado adminLTE a pdf

--- resources/views/admin/configuracion/transporte/index.blade.php
+++ resources/views/admin/configuracion/transporte/index.blade.php
@@ -9,7 +9,7 @@
         <div class="col-xs-12">
           <div class="box">
             <div class="box-header">
-              <h3 class="box-title">Seccion de Usuarios</h3>
+              <h3 class="box-title">Configuracion Transporte</h3>
             </div>
 			<div class="box-body">
 
--- resources/views/pdf/listado_reportes.blade.php
+++ resources/views/pdf/listado_reportes.blade.php
@@ -5,13 +5,17 @@
 <!-- muestra mensaje que se a modificado o creado exitosamente-->
 @include('alerts.success')
 
-<div class="panel-body">
-<div class="container-fluid">
-
+<section class="content">
+      <div class="row">
+        <div class="col-xs-12">
+          <div class="box">
+            <div class="box-header">
+              <h3 class="box-title">Reportes</h3>
+            </div>
+      <div class="box-body">
  
 
-
-<table class="table">
+<table id="example2" class="table table-bordered table-hover">
   <thead>
     <th>ID</th>
     <th>reporte</th>
@@ -26,18 +30,18 @@
     <td>Reporte de Usuarios por Pais</td>
     <td><a href="crear_reporte_porpais/1" target="_blank" ><button class="btn btn-block btn-primary btn-xs">Ver</button></a></td>
     <td><a href="crear_reporte_porpais/2" target="_blank" ><button class="btn btn-block btn-success btn-xs">Descargar</button></a></td>
-                    
+              
   </tr>
- 
-
-
-
   </tbody>
-
   </table>
-
-
  
-</div>
-</div>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </div>
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+    </section>
 @endsection
