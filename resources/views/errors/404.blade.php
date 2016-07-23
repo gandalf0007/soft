1e403f7b8eb0bb560cfd66cbd831e36b0a931c68
matiascorrea007 <matiascorreautn@gmail.com>
2016-07-23 07:12:21 +0200

    fix 404 error

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,34 +1,23 @@
-@extends('layouts.app')
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Error 404 - SharkInformatica</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<link rel="stylesheet" href="{{ asset('/css/404.css') }}">
+</head>
+<body>
+<div class="wrap">
+	
+	<div class="banner">
+		<img src="images/banner.jpg" alt="" />
+	</div>
+	<div class="page">
+		<h2>Oops! Page Not Found</h2>
+	</div>
+	<div class="footer">
+	</div>
+</div>
+</body>
+</html>
 
-@section('htmlheader_title')
-    Page not found
-@endsection
-
-@section('contentheader_title')
-    404 Error Page
-@endsection
-
-@section('$contentheader_description')
-@endsection
-
-@section('main-content')
-
-<div class="error-page">
-    <h2 class="headline text-yellow"> 404</h2>
-    <div class="error-content">
-        <h3><i class="fa fa-warning text-yellow"></i> Oops! Page not found.</h3>
-        <p>
-            We could not find the page you were looking for.
-            Meanwhile, you may <a href='{{ url('/home') }}'>return to dashboard</a> or try using the search form.
-        </p>
-        <form class='search-form'>
-            <div class='input-group'>
-                <input type="text" name="search" class='form-control' placeholder="Search"/>
-                <div class="input-group-btn">
-                    <button type="submit" name="submit" class="btn btn-warning btn-flat"><i class="fa fa-search"></i></button>
-                </div>
-            </div><!-- /.input-group -->
-        </form>
-    </div><!-- /.error-content -->
-</div><!-- /.error-page -->
-@endsection
